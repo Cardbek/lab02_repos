1a9abf275043d7524cf6a23158037f13fe87cbdd
unknown <sashkocenyuk@gmail.com>
2025-09-17 22:10:23 +0300

    first master commit

---
+++ pr2/Lab_02.cpp
@@ -0,0 +1,32 @@
+// Lab_02.cpp
+// Ценюк Олександр
+// Лабораторна робота No
+// 2.
+// Лінійні програми.
+// Варіант 26
+
+#include <iostream>
+#include <cmath>   // для cos()
+
+using namespace std;
+
+int main() {
+    double a;   // вхідний параметр
+    double z1;  // результат першого виразу
+   // double z2;  // результат другого виразу
+
+    cout << "a = ";
+    cin >> a;
+
+    // Обчислення
+    z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
+   // z2 = 4 * cos(a / 2) * cos(5 * a / 2) * cos(4 * a);
+
+    // Вивід
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+   // cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
